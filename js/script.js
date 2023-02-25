7851723901e5f29d53603608341118ba3d3b1e3e
Ahmedwaleed22 <ahmedwaleedaboelamayem68@gmail.com>
2023-02-25 21:22:52 +0200

    Adding react app

--- js/script.js
+++
@@ -1,8 +0,0 @@
-var amount = document.getElementById("amount");
-function toggle() {
-  if (amount.value > 50000) {
-    location.href = "/reportform";
-  } else {
-    alert("Enter a valid amount");
-  }
-}
